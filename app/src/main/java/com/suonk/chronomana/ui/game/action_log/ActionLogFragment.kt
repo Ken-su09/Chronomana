726b2b5f70c9049cbeb44312b8421256fb5a6d5b
Ken Suon <ken.su_dev@hotmail.com>
2024-12-06 14:18:43 +0100

    Clean code

---
+++ app/src/main/java/com/suonk/chronomana/ui/game/action_log/ActionLogFragment.kt
@@ -0,0 +1,4 @@
+package com.suonk.chronomana.ui.game.action_log
+
+class ActionLogFragment {
+}
--- app/src/main/java/com/suonk/chronomana/ui/selection/PlayersViewAction.kt
+++
@@ -1,8 +0,0 @@
-package com.suonk.chronomana.ui.selection
-
-sealed class PlayersViewAction {
-
-    sealed class Navigate : PlayersViewAction() {
-        class PlayerDetails(id: String) : Navigate()
-    }
-}
